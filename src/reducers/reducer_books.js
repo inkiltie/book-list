f41ed1b3d78e4205e09b3a69997acc346b3665c5
Nikita Tolpekin <vancoder@Vancoder-MacBook-Pro.local>
2018-05-12 00:14:55 -0700

    Innitial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'JavaScript: The Good Parts', pages: 774 },
+        { title: 'Harry Potter', pages: 244 },
+        { title: 'Dark Tower', pages: 412 },
+        { title: 'Meteor for Beginners', pages: 894 }
+    ];
+}
